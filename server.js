5b3f50f80e5550c36608b162c9ddf268441bab4a
Ali Imran <ali786vii@gmail.com>
2025-01-21 17:11:23 +0500

    database connection

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express"
+import dotenv from "dotenv"
+import connectToDB from "./database/db.js"
+
+dotenv.config();
 const app = express()
 const port = process.env.port || 4000;
 
 // middleware
 app.use(express.json())
+
+connectToDB();
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
